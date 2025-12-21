4e4dda427ce625ecff4f24b43e24333ae0d47d98
Stéphanie Coupama <frimouss.graphweb@gmail.com>
2025-12-21 12:26:12 +0400

    Création d'un dossier data et mise en place du fichier logements.json + installation de sass + mise en place du test pour bon fonctionnement de react-router-dom

---
+++ src/routerTest.jsx
@@ -0,0 +1,13 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// function RouterTest() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<h1>Test Router Opérationnel</h1>} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default RouterTest;
