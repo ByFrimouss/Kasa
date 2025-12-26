9deabc2c999338fc530331845869e291ac9ff29b
Stéphanie Coupama <frimouss.graphweb@gmail.com>
2025-12-26 17:35:50 +0400

    Mise en place de la page d'erreur 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import "../styles/notFound.scss";
+
 function NotFound() {
-  return <h1>404 - Page non trouvée</h1>;
+  return (
+    <div className="notfound-container">
+      <h1 className="notfound-title">404</h1>
+      <p className="notfound-text">
+        Oups ! La page que vous cherchez n’existe pas.
+      </p>
+      <Link to="/" className="notfound-button">
+        Retourner sur page d'accueil
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
